2d10b677b9fd17a94d7586c92966654fba822615
ExHunterEx <yahyaagung65@gmail.com>
2025-01-02 00:48:49 +0700

    Delete Frontend directory

--- Frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import "./App.css";
-import { routes } from "./routes";
-
-function App() {
-  return <RouterProvider router={createBrowserRouter(routes)} />;
-}
-
-export default App;
